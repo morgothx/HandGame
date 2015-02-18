8864c2e2a6dba1497c4eb6565039f79ada53eff0
Andres Lopez <andres.lopez@brainz.co>
2015-02-18 18:41:00 -0500

    Add tests moves rules

--- Assets/Editor/HandGameTest.cs
+++ Assets/Editor/HandGameTest.cs
@@ -23,25 +23,113 @@ public class HandGameTest
 		handGame.Play (player1, player2);
 	}
 	
+	private void AssertPlayer1Wins(Move player1Move, Move player2Move)
+	{
+		Play (player1Move, player2Move);
+		Assert.AreEqual (1, player1.VictoriesCount);
+		Assert.AreEqual (0, player2.VictoriesCount);
+	}
+	
+	private void AssertPlayer2Wins(Move player1Move, Move player2Move)
+	{
+		Play (player1Move, player2Move);
+		Assert.AreEqual (0, player1.VictoriesCount);
+		Assert.AreEqual (1, player2.VictoriesCount);
+	}
+	
 	[Test]
 	public void AssertPaperBeatsRock ()
 	{
-		Play (Move.Paper, Move.Rock);
-		Assert.AreEqual (1, player1.VictoriesCount);
-		Assert.AreEqual (0, player2.VictoriesCount);
+		AssertPlayer1Wins (Move.Paper, Move.Rock);
+	}
+	
+	[Test]
+	public void AssertPaperBeatsSpock ()
+	{
+		AssertPlayer1Wins (Move.Paper, Move.Spock);
+	}
+	
+	[Test]
+	public void AssertPaperIsBeatedByScissors ()
+	{
+		AssertPlayer2Wins (Move.Paper, Move.Scissors);
+	}
+	
+	[Test]
+	public void AssertPaperIsBeatedByLizzard ()
+	{
+		AssertPlayer2Wins (Move.Paper, Move.Lizzard);
+	}
+	
+	[Test]
+	public void AssertRockBeatsScissors ()
+	{
+		AssertPlayer1Wins (Move.Rock, Move.Scissors);
+	}
+	
+	[Test]
+	public void AssertRockBeatsLizzard ()
+	{
+		AssertPlayer1Wins (Move.Rock, Move.Lizzard);
+	}
+	
+	[Test]
+	public void AssertRockIsBeatedByPaper ()
+	{
+		AssertPlayer2Wins (Move.Rock, Move.Paper);
+	}
+	
+	[Test]
+	public void AssertRockIsBeatedBySpock ()
+	{
+		AssertPlayer2Wins (Move.Rock, Move.Spock);
 	}
 	
 	[Test]
 	public void AssertScissorsBeatsPaper ()
 	{
-		IPlayer player1 = Substitute.For<IPlayer> ();
-		player1.Move.Returns (Move.Paper);
-		
-		IPlayer player2 = Substitute.For<IPlayer> ();
-		player2.Move.Returns (Move.Scissors);
-		
-		handGame.Play (player1, player2);
-		Assert.AreEqual (0, player1.VictoriesCount );
-		Assert.AreEqual (1, player2.VictoriesCount );
+		AssertPlayer1Wins (Move.Scissors, Move.Paper);
+	}
+	
+	[Test]
+	public void AssertScissorsBeatsLizzard ()
+	{
+		AssertPlayer1Wins (Move.Scissors, Move.Lizzard);
+	}
+	
+	[Test]
+	public void AssertScissorsIsBeatedByRock ()
+	{
+		AssertPlayer2Wins (Move.Scissors, Move.Rock);
+	}
+	
+	[Test]
+	public void AssertScissorsIsBeatedBySpock ()
+	{
+		AssertPlayer2Wins (Move.Scissors, Move.Spock);
+	}
+	
+	[Test]
+	public void AssertSpockBeatsScissors ()
+	{
+		AssertPlayer1Wins (Move.Spock, Move.Scissors);
+	}
+	
+	[Test]
+	public void AssertSpockBeatsRock ()
+	{
+		AssertPlayer1Wins (Move.Spock, Move.Rock);
+	}
+	
+	[Test]
+	public void AssertSpocksIsBeatedByPaper ()
+	{
+		AssertPlayer2Wins (Move.Spock, Move.Paper);
+	}
+	
+	[Test]
+	public void AssertSpockIsBeatedByLizzard ()
+	{
+		AssertPlayer2Wins (Move.Spock, Move.Lizzard);
 	}
 }
--- Assets/Scripts/HandGame.cs
+++ Assets/Scripts/HandGame.cs
@@ -17,9 +17,45 @@ public class HandGame
 	
 	public void Play (IPlayer player1, IPlayer player2)
 	{
-		if (player2.Move == Move.Scissors)
-			player2.VictoriesCount = 1;
-		else
-			player1.VictoriesCount = 1;
+		if (player1.Move == Move.Paper && player2.Move == Move.Rock)
+			WinRound(player1);
+		if (player1.Move == Move.Paper && player2.Move == Move.Spock)
+			WinRound(player1);
+		if (player1.Move == Move.Paper && player2.Move == Move.Scissors)
+			WinRound(player2);
+		if (player1.Move == Move.Paper && player2.Move == Move.Lizzard)
+			WinRound(player2);
+		
+		if (player1.Move == Move.Rock && player2.Move == Move.Scissors)
+			WinRound(player1);
+		if (player1.Move == Move.Rock && player2.Move == Move.Lizzard)
+			WinRound(player1);
+		if (player1.Move == Move.Rock && player2.Move == Move.Paper)
+			WinRound(player2);
+		if (player1.Move == Move.Rock && player2.Move == Move.Spock)
+			WinRound(player2);
+		
+		if (player1.Move == Move.Scissors && player2.Move == Move.Paper)
+			WinRound(player1);
+		if (player1.Move == Move.Scissors && player2.Move == Move.Lizzard)
+			WinRound(player1);
+		if (player1.Move == Move.Scissors && player2.Move == Move.Rock)
+			WinRound(player2);
+		if (player1.Move == Move.Scissors && player2.Move == Move.Spock)
+			WinRound(player2);
+		
+		if (player1.Move == Move.Spock && player2.Move == Move.Scissors)
+			WinRound(player1);
+		if (player1.Move == Move.Spock && player2.Move == Move.Rock)
+			WinRound(player1);
+		if (player1.Move == Move.Spock && player2.Move == Move.Paper)
+			WinRound(player2);
+		if (player1.Move == Move.Spock && player2.Move == Move.Lizzard)
+			WinRound(player2);
+	}
+	
+	private void WinRound(IPlayer player)
+	{
+		player.VictoriesCount++;
 	}
 }
